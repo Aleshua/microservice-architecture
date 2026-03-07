af76b4737be0dba22ee340c275419bfcc0c069ed
Alexey Senachin <senachin@a2design.biz>
2026-03-07 22:22:32 +0600

    add article usecases and dependency injection

--- src/dependencies/usecases.py
+++ src/dependencies/usecases.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
 
-from src.dependencies.repositories import get_user_repository
+from src.dependencies.repositories import get_article_repository, get_user_repository
 from src.dependencies.security import get_password_service, get_token_service
+from src.repositories.article import ArticleRepository
 from src.repositories.user import UserRepository
 from src.services.password import PasswordService
 from src.services.token import TokenService
+from src.usecases.article import ArticleUseCases
 from src.usecases.user import UserUseCases
 
 
@@ -14,3 +16,9 @@ def get_user_usecases(
     token_service: TokenService = Depends(get_token_service),
 ) -> UserUseCases:
     return UserUseCases(repository, password_service, token_service)
+
+
+def get_article_usecases(
+    repository: ArticleRepository = Depends(get_article_repository),
+) -> ArticleUseCases:
+    return ArticleUseCases(repository)
