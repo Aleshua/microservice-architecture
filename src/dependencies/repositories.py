6f7829395a6eaa9dea16cf2c33f176eab306493a
Alexey Senachin <senachin@a2design.biz>
2026-03-07 21:52:26 +0600

    add dependency injection for services, repositories and usecases

---
+++ src/dependencies/repositories.py
@@ -0,0 +1,11 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_session
+from src.repositories.user import UserRepository
+
+
+def get_user_repository(
+    session: AsyncSession = Depends(get_session),
+) -> UserRepository:
+    return UserRepository(session)
