e717fa77de533a865e20b41994349a939d48baae
Alexey Senachin <senachin@a2design.biz>
2026-03-07 21:51:24 +0600

    add user schemas with dto, responses and api response wrapper

---
+++ src/schemas/user/dto/__init__.py
@@ -0,0 +1,5 @@
+from src.schemas.user.dto.create import UserCreate
+from src.schemas.user.dto.login import UserLogin
+from src.schemas.user.dto.update import UserUpdate
+
+__all__ = ["UserCreate", "UserLogin", "UserUpdate"]
