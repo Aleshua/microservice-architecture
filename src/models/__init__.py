28b333d284463aed605400ea1f843f2801943a18
Alexey Senachin <senachin@a2design.biz>
2026-03-07 22:43:11 +0600

    add comment model with author and article relationships

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from src.models.base import Base
 from src.models.user import User
 from src.models.article import Article
+from src.models.comment import Comment
 
-__all__ = ["Base", "User", "Article"]
+__all__ = ["Base", "User", "Article", "Comment"]
