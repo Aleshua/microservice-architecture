9b339b2f723fa0f71e8c577f896e2aac7d02435b
Alexey Senachin <senachin@a2design.biz>
2026-03-08 18:00:23 +0600

    add POST /users/subscribe endpoint with subscription usecases

--- users_service/src/dependencies/usecases.py
+++ users_service/src/dependencies/usecases.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
 
-from src.dependencies.repositories import get_user_repository
+from src.dependencies.repositories import get_subscriber_repository, get_user_repository
 from src.dependencies.security import get_password_service, get_token_service
+from src.repositories.subscriber import SubscriberRepository
 from src.repositories.user import UserRepository
 from src.services.password import PasswordService
 from src.services.token import TokenService
+from src.usecases.subscription import SubscriptionUseCases
 from src.usecases.user import UserUseCases
 
 
@@ -14,3 +16,10 @@ def get_user_usecases(
     token_service: TokenService = Depends(get_token_service),
 ) -> UserUseCases:
     return UserUseCases(repository, password_service, token_service)
+
+
+def get_subscription_usecases(
+    user_repository: UserRepository = Depends(get_user_repository),
+    subscriber_repository: SubscriberRepository = Depends(get_subscriber_repository),
+) -> SubscriptionUseCases:
+    return SubscriptionUseCases(user_repository, subscriber_repository)
