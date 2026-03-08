2e94526ba5f58b94b103f67430e0df94f639ec9b
Alexey Senachin <senachin@a2design.biz>
2026-03-08 18:00:16 +0600

    add subscribe request dto and subscription exceptions

--- users_service/src/schemas/user/__init__.py
+++ users_service/src/schemas/user/__init__.py
@@ -1,4 +1,4 @@
-from src.schemas.user.dto import SubscriptionKeyUpdate, UserCreate, UserLogin, UserUpdate
+from src.schemas.user.dto import SubscribeRequest, SubscriptionKeyUpdate, UserCreate, UserLogin, UserUpdate
 from src.schemas.user.responses import AuthResponse, TokenResponse, UserResponse
 
-__all__ = ["SubscriptionKeyUpdate", "UserCreate", "UserLogin", "UserUpdate", "AuthResponse", "TokenResponse", "UserResponse"]
+__all__ = ["SubscribeRequest", "SubscriptionKeyUpdate", "UserCreate", "UserLogin", "UserUpdate", "AuthResponse", "TokenResponse", "UserResponse"]
